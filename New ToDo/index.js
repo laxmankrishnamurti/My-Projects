99d37cde28dde828a981b3584c1616ddd354fa06
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-02-22 14:07:48 +0530

    Deleting some files

--- New ToDo/index.js
+++
@@ -1,8 +0,0 @@
-const displayTime = document.getElementById('display-time');
-const displayDate = document.getElementById('display-date');
-
-setInterval(() => {
-    let today = new Date();
-    displayTime.textContent = today.toLocaleTimeString();
-    displayDate.textContent = today.toLocaleDateString();
-}, 1000);
