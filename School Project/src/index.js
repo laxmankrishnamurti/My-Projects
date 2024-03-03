f457f612b3b2ee78b314376ed3e7d610af385d7c
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-03-03 17:34:18 +0530

    Starting the School Project

--- School Project/src/index.js
+++ School Project/src/index.js
@@ -1,10 +1,12 @@
 const menuButton = document.querySelector('.menu-btn-container');
 const navbarWrapper = document.querySelector('.navbar-wrapper')
 
+//Show navigation-bar on Click
 function showMenu() {
     navbarWrapper.style.left = '0%';
 }
 
+//Hide navigation-bar on Click
 function hideMenu() {
     navbarWrapper.style.left = '100%'
 }
