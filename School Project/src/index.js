ef7e42a64834321d744c7ef659a9667ccbc8f2d7
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-03-13 18:06:07 +0530

    Making changes while doing project

--- School Project/src/index.js
+++ School Project/src/index.js
@@ -1,3 +1,5 @@
+// SCRIPT :: Navigation Bar 
+
 const menuButton = document.querySelector('.menu-btn-container');
 const navbarWrapper = document.querySelector('.navbar-wrapper')
 
@@ -10,3 +12,49 @@ function showMenu() {
 function hideMenu() {
     navbarWrapper.style.left = '100%'
 }
+
+// SCRIPT :: Crousal (Student's list ) starts here 
+
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+const profile = document.querySelector('.profile');
+const images = document.querySelectorAll('.images');
+
+let scroll = 1;
+const length = images.length;
+
+const nextSlide = () => {
+    profile.style.transform = `translateX(-${scroll * 340}px)`;
+    scroll++;
+}
+
+const getFirstSlide = () => {
+    profile.style.transform = `translateX(0px)`;
+    scroll = 1;
+}
+
+const prevSlide = () => {
+    profile.style.transform = `translateX(-${(scroll - 2) * 340}px)`;
+    scroll--;
+}
+
+const getLastSlide = () => {
+    profile.style.transform = `translateX(-${(length - 3) * 340}px)`;
+    scroll = lengths;
+}
+
+rightArrow.addEventListener('click', () => {
+    if (scroll < length - 2) {
+        nextSlide();
+    } else {
+        getFirstSlide();
+    }
+})
+
+leftArrow.addEventListener('click', () => {
+    if (scroll > 1) {
+        prevSlide();
+    } else {
+        getLastSlide();
+    }
+})
